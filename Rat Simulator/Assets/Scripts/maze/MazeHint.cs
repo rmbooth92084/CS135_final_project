f1643d2fa55ae560c15b473f3024443ea24bf64a
eyao004 <eyao004@ucr.edu>
2022-03-09 00:26:29 -0800

    Update
    Based from older main branch. Created the setup for the dodge puzzle (broken code commented out) and completed the maze puzzle (text display needs to be tied to only when the player is in the maze area).

---
+++ Rat Simulator/Assets/Scripts/maze/MazeHint.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeHint : MonoBehaviour {
+    public GameObject finish;
+    public GameObject player;
+    private Vector2 finish_position;
+    private Vector2 player_position;
+    private UnityEngine.UI.Text hint_text;
+    private float distance_to_goal;
+
+    void Start() {
+        hint_text = this.GetComponent<UnityEngine.UI.Text>();
+        finish_position = new Vector2(finish.transform.position.x, finish.transform.position.z);
+    }
+
+    void Update() {
+        player_position = new Vector2(player.transform.position.x, player.transform.position.z);
+        distance_to_goal = Vector2.Distance(finish_position, player_position);
+
+        if (distance_to_goal < 1.5) {
+            hint_text.text = "Finish!";
+            hint_text.color = Color.green;
+        }
+        else if (distance_to_goal < 3) {
+            hint_text.text = "Close!!!";
+            hint_text.color = Color.green;
+        }
+        else if (distance_to_goal < 5) {
+            hint_text.text = "Getting closer!!";
+            hint_text.color = Color.green;
+        }
+        else if (distance_to_goal < 8) {
+            hint_text.text = "Getting closer!";
+            hint_text.color = new Color(1.0f, 0.64f, 0.0f);
+        }
+        else {
+            hint_text.text = "Not close";
+            hint_text.color = Color.red;
+        }
+    }
+}
